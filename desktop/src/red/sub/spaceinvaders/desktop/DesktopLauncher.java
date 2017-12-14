d5af00e57f2b693b0c7b7c320a59b7800e74422e
Marlin <marlin.jurczik@live.com>
2017-12-14 14:29:54 +0100

    init commit

---
+++ desktop/src/red/sub/spaceinvaders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package red.sub.spaceinvaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import red.sub.spaceinvaders.SpaceInvaders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceInvaders(), config);
+	}
+}
